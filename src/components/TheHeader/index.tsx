887b9db6ca8f72680d165173c87083ada5a40d74
Shumpei <shumpei.o.7g@gmail.com>
2023-08-13 10:37:00 +0900

    feat: TOP FV位置調整

--- src/components/TheHeader/index.tsx
+++ src/components/TheHeader/index.tsx
@@ -1,19 +1,28 @@
-import classNames from "classnames"
-import { ComponentProps } from "react"
+import classNames from 'classnames';
+import { ComponentProps } from 'react';
 
 type Props = {
-  isShowInner?: boolean
-} & ComponentProps<"header">
+  isShowInner?: boolean;
+} & ComponentProps<'header'>;
 
-export const TheHeader: React.FC<Props> = ({ isShowInner = true, className }) => {
+export const TheHeader: React.FC<Props> = ({
+  isShowInner = true,
+  className,
+}) => {
   return (
-    <header className={classNames([className, "fixed z-10 w-full", !isShowInner ? "blur-md" : "blur-none"])}>
-      <div className="container mx-auto">
+    <header
+      className={classNames([
+        className,
+        'fixed z-10 w-full',
+        !isShowInner ? 'blur-md' : 'blur-none',
+      ])}
+    >
+      <div className="container mx-auto mt-16">
         <hgroup className="flex flex-row items-end">
-          <h1 className={classNames(["text-184 leading-[0.75]"])}>MB.js</h1>
+          <h1 className={classNames(['text-184 leading-[0.75]'])}>MB.js</h1>
           <span className=" leading-4">Shumpei's Portfolio site</span>
         </hgroup>
       </div>
     </header>
-  )
-}
+  );
+};
--- src/layout/home/MainVisual/index.tsx
+++ src/layout/home/MainVisual/index.tsx
@@ -1,31 +1,55 @@
-import { Count } from "@/layout/home/Count"
-import { MainVisualArrowDown } from "@/features/home/MainVisualArrowDown";
-import { ComponentProps } from "react"
-import classNames from "classnames";
+import { Count } from '@/layout/home/Count';
+import { ComponentProps } from 'react';
+import classNames from 'classnames';
 
-type Props = ComponentProps<typeof Count>
+type Props = ComponentProps<typeof Count>;
 
-export const MainVisual: React.FC<Props> = ({ isShowInner, handleShowInner }
-  ) => {
+export const MainVisual: React.FC<Props> = ({
+  isShowInner,
+  handleShowInner,
+}) => {
   return (
     <section data-section="MainVisual" className="relative">
-      <div className="absolute top-0">
-        <Count isShowInner={isShowInner} handleShowInner={handleShowInner} />
-        <div className={classNames(["absolute duration-[2800ms] left-1/4 -translate-x-1/4", isShowInner ? "opacity-100 top-0 blur-none" : "opacity-0 -top-[600px] blur-md"])}>
-          <div className={classNames(["absolute"])}>
+      <Count isShowInner={isShowInner} handleShowInner={handleShowInner} />
+      <div className="relative container mx-auto">
+        <div
+          className={classNames([
+            'container mx-auto absolute duration-[2800ms] left-1/4 -translate-x-1/4',
+            isShowInner
+              ? 'opacity-100 top-0 blur-none'
+              : 'opacity-0 -top-[600px] blur-md',
+          ])}
+        >
+          <div className="absolute left-[10%]">
             <div className="w-500">
-              <img src={"/images/arm_and_sign.png"} alt="" className="w-full h-auto opacity-80"/>
+              <img
+                src={'/images/arm_and_sign.png'}
+                alt=""
+                className="w-full h-auto opacity-80"
+              />
             </div>
           </div>
         </div>
-        <div className={classNames(["absolute right-[3vw] top-[45.6vh] duration-[2800ms]", isShowInner ? "opacity-100 top-0 blur-none" : "opacity-0 -top-[600px] blur-md"])}>
+        <div
+          className={classNames([
+            'absolute right-0 top-[45.6vh] duration-[2800ms]',
+            isShowInner
+              ? 'opacity-100 top-0 blur-none'
+              : 'opacity-0 -top-[600px] blur-md',
+          ])}
+        >
           <hgroup>
-            <h2 className="uppercase text-40">Web front-end developer<br/>& Doujin creator</h2>
-            <p className="text-20 before:content-['{'] before:pr-1 after:content-['}'] after:pl-1">Illustration : Graphic Design : Manga</p>
+            <h2 className="uppercase text-40">
+              Web front-end developer
+              <br />& Doujin creator
+            </h2>
+            <p className="text-20 before:content-['{'] before:pr-1 after:content-['}'] after:pl-1">
+              Illustration : Graphic Design : Manga
+            </p>
           </hgroup>
+          <span className="arrow down" />
         </div>
-        <MainVisualArrowDown />
       </div>
     </section>
-  )
-}
+  );
+};
