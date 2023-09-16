c6529976d7b2005f678e1aec1ea1264d485c42f0
Shumpei <shumpei.o.7g@gmail.com>
2023-09-16 17:08:23 +0900

    feat: RecentProject のマーキースタイル修正

--- src/layout/home/MarqueeRecentProject/index.tsx
+++ src/layout/home/MarqueeRecentProject/index.tsx
@@ -1,7 +1,7 @@
 export const MarqueeRecentProject: React.FC = () => {
   return (
-    <div className="w-screen h-120 my-0 mx-[calc(50%_-_50vw)] overflow-hidden mt-32 border-b-1 border-b-primary">
-      <div
+    <div className="w-screen h-120 my-0 mx-[calc(50%_-_50vw)] overflow-hidden mt-32 border-b-1 border-b-primary flex items-center">
+      {/* <div
         data-scroll="parent"
         className="relative w-screen h-[inherit] my-0 mx-[calc(50%_-_50vw)] overflow-hidden"
       >
@@ -11,35 +11,28 @@ export const MarqueeRecentProject: React.FC = () => {
         >
           <span className="text-primary uppercase text-100">
             Recent Project
+            <span className="text-primary uppercase text-80">
+              &nbsp;/&nbsp;
+            </span>
           </span>
-          <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
-          <span className="text-primary uppercase text-100">
-            Recent Project
-          </span>
-          <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
-          <span className="text-primary uppercase text-100">
-            Recent Project
-          </span>
-          <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
         </div>
-        <div
-          data-scroll="child-second"
-          className="flex flex-row items-center justify-around absolute left-0 top-0 w[inherit] h-[inherit] marquee-child whitespace-nowrap"
-        >
-          <span className="text-primary uppercase text-100">
-            Recent Project
-          </span>
-          <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
-          <span className="text-primary uppercase text-100">
-            Recent Project
-          </span>
-          <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
-          <span className="text-primary uppercase text-100">
-            Recent Project
-          </span>
-          <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
-        </div>
-      </div>
+      </div> */}
+      <span className="text-primary uppercase text-100 whitespace-nowrap marquee-right items-center flex">
+        <span>Recent Project</span>
+        <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
+      </span>
+      <span className="text-primary uppercase text-100 whitespace-nowrap marquee-right items-center flex">
+        <span>Recent Project</span>
+        <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
+      </span>
+      <span className="text-primary uppercase text-100 whitespace-nowrap marquee-right items-center flex">
+        <span>Recent Project</span>
+        <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
+      </span>
+      <span className="text-primary uppercase text-100 whitespace-nowrap marquee-right items-center flex">
+        <span>Recent Project</span>
+        <span className="text-primary uppercase text-80">&nbsp;/&nbsp;</span>
+      </span>
     </div>
   );
 };
